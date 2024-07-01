9fba3b0babcec978f03dd97c3e0f4cb831a0f033
an0209vn <48006180+an0209vn@users.noreply.github.com>
2024-07-01 15:23:18 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -117,6 +117,8 @@ $('.slider-banner').owlCarousel({
     loop:false,
     margin:0,
     nav:false,
+    autoplay:true,
+    autoplayTimeout:3000,
     responsive:{
         0:{
             items:1
